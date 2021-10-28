0589d56a1ed5289010d7d465e3224f0917f20ead
sahithi2121 <93140382+sahithi2121@users.noreply.github.com>
2021-10-28 18:11:24 -0500

    Time: 6 ms (83.61%), Space: 39.8 MB (56.78%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.java
+++ valid-palindrome-ii/valid-palindrome-ii.java
@@ -2,9 +2,10 @@ class Solution {
     public boolean validPalindrome(String s) {
         int left =0;
         int right = s.length()-1;
-        while(left<right){
-        if(s.charAt(left) != s.charAt(right))
-            return checkPalindrome(left+1,right,s) || checkPalindrome(left,right-1,s);
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+                 return checkPalindrome(left+1,right,s) || checkPalindrome(left,right-1,s);
                 left++;
                 right--;
         }return true;
@@ -14,6 +15,6 @@ class Solution {
             if(s.charAt(left++) != s.charAt(right--))
                 return false;
         }return true;
+        }
         
     }
-}
