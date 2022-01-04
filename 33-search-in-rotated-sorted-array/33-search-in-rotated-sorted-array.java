bee809ae8d3b329d6cefc2164b07984336cca1ba
sahithi2121 <93140382+sahithi2121@users.noreply.github.com>
2022-01-03 21:35:33 -0600

    Time: 0 ms (100.00%), Space: 38.5 MB (37.66%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 0 || nums == null) return -1;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == target)
+                return i;
+        } return -1;
+    }
+}
